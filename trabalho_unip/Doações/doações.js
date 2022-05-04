c1148a7d7ab0e847dbb69ace2844884a96b4378c
Italo Moura <97275782+Italoomoura@users.noreply.github.com>
2022-05-04 17:19:07 -0300

    Add files via upload

--- trabalho_unip/Doações/doações.js
+++ trabalho_unip/Doações/doações.js
@@ -10,6 +10,29 @@ function mostrar(doar1, doar2) {
 	
 }
 
+function Enviar() {
+    var nome = document.getElementById("nome");
+    var valor = document.getElementById("valor");
+    if (valor.value > 1) {
+        var moeda = "reais";
+    }
+    else {
+        var moeda = "real";
+    }
+    if (valor.value < 1) {
+        document.write("<head><link rel='stylesheet' href='PaginaDoacao.css'></head><div class='container'><h1 style='text-align: center;'>Por favor, insira um valor!</h1></div>");
+    }
+    else {
+        if (nome.value != "") {
+            document.write(
+                "<head><link rel='stylesheet' href='PaginaDoacao.css'></head><div class='container'><h1 style='text-align: center;'>Obrigado Por Doar "+valor.value+" "+moeda+", Senhor "+nome.value+"!</h1></div>");
+        }
+        else {
+            document.write("<head><link rel='stylesheet' href='PaginaDoacao.css'></head><div class='container'><h1 style='text-align: center;'>Obrigado Por Doar "+valor.value+" "+moeda+"!</h1></div>");
+        }
+    }
+}
+
 function fim(doar2, fim) {
 
     var div3 = document.getElementById(fim);
@@ -19,5 +42,5 @@ function fim(doar2, fim) {
 	var div2 = document.getElementById(doar2);
 	var disp2 = div2.style.display;
     div2.style.display = disp2 == 'none' ? 'block' : 'none';
-
+    Enviar();
 }
